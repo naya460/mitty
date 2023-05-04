a504e0c7d53a1e21cdc0970d1da20382264c5d76
naya460 <76655448+naya460@users.noreply.github.com>
2023-05-05 00:41:48 +0900

    prismaを使用してデータベースを作成

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,10 +1,18 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
+import prisma, { Prisma } from '../../lib/prisma'
 
 export default withIronSessionApiRoute(
     async function loginRoute(req, res) {
         console.log('body: ', req.body);
         await req.session.save();
-        res.status(200).send('ok');
+        const user = await prisma.user.create({
+            data: {
+                user_name: req.body.user_name,
+                password: req.body.password
+            }
+        });
+        const data = await prisma.user.findMany();
+        res.status(200).json({data});
     }, {
         cookieName: 'session-id',
         password: 'z8Cpj2nNv9VPdsxZxostDu5ufXxme2V0',
