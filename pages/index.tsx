b703b57f3a745465a1ec0778f0f4958d3d95da27
naya460 <76655448+naya460@users.noreply.github.com>
2023-05-05 17:00:32 +0900

    submitの処理を関数化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
 import Link from 'next/link'
 
 export default function IndexPage() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      user_name: event.target.user_name.value,
+      password: event.target.password.value,
+    };
+
+    const JSONdata = JSON.stringify(data);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch('api/login', options);
+
+    const result = await response.json();
+    console.log(result.data)
+  }
   return (
-    <form action='/api/login' method='post'>
+    <form onSubmit={handleSubmit}>
       <label>User Name:</label><br />
       <input type='text' name='user_name' required/><br />
       <label>Password:</label><br />
