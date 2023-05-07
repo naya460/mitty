d1d14afbda88e2313ea22423d5fa64356659bda2
naya460 <76655448+naya460@users.noreply.github.com>
2023-05-07 22:21:50 +0900

    メッセージを取得

--- components/main_page/index.tsx
+++ components/main_page/index.tsx
@@ -1,3 +1,4 @@
+import react, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 interface Props {
@@ -6,12 +7,33 @@ interface Props {
 
 export default function MainPage(props: Props) {
   const router = useRouter();
+  const [messages, setMessages] = useState();
+  const [displayMessages, setDisplayMessages] = useState(null);
 
   // サインアウト処理
   const handleSignOut = async () => {
     await fetch('/api/user/signout');
     router.reload();
-  }  
+  }
+
+  useEffect(() => {
+    (async () => {
+      // メッセージを取得する
+      const res = await fetch('api/message/get');
+      const msg = await res.json();
+      setMessages(msg);
+      // メッセージの表示を作成
+      let display_msg = [];
+      for (let i in msg) {
+        display_msg.push(
+          <p key={i} style={{whiteSpace: 'pre-line'}}>
+            {msg[i].message_text}<br/>[{msg[i].author.user_name}]
+          </p>
+        );
+      }
+      setDisplayMessages(display_msg);
+    })()
+  }, [])
 
   return (
     <>
@@ -21,6 +43,7 @@ export default function MainPage(props: Props) {
         <textarea name='message' autoComplete='off' style={{resize: 'none'}} required/><br/>
         <button type='submit'>Send</button>
       </form>
+      <div>{displayMessages}</div>
     </>
   );
 }
---
+++ pages/api/message/get.ts
@@ -0,0 +1,19 @@
+import prisma from 'lib/prisma'
+import { withSessionRoute } from 'lib/withSession'
+
+export default withSessionRoute(UserRoute);
+
+async function UserRoute(req, res) {
+  // メッセージを取得
+  let messages = await prisma.message.findMany({
+    select: {
+      message_text: true,
+      author: {
+        select: {
+          user_name: true
+        }
+      }
+    }
+  });
+  res.status(200).json(messages);
+}
