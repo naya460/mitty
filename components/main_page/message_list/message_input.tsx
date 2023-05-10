8e4fde15713a883cbd87e30eb1ca3ba036143ffd
naya460 <76655448+naya460@users.noreply.github.com>
2023-05-10 22:02:42 +0900

    メッセージ入力欄の高さを自動調節

--- components/main_page/message_list/message_input.tsx
+++ components/main_page/message_list/message_input.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router'
+import react, { useState } from 'react'
 
 import styles from './message_input.module.css'
 
 export default function MessageInput() {
   const router = useRouter();
+  const [text, setText] = useState('');
+  const [lineCount, setLineCount] = useState(1);
 
   const handleSend = async (event) => {
     event.preventDefault();
@@ -28,10 +31,24 @@ export default function MessageInput() {
     router.reload();
   }
 
+  const handleChange: react.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
+    setText(event.target.value);
+    const length = event.target.value.split('\n').length;
+    setLineCount(Math.min(length, 5));
+  }
+
   return (
     <div className={styles.top}>
       <form onSubmit={handleSend} className={styles.form}>
-        <textarea name='message' autoComplete='off' className={styles.message_box} rows={1} required/><br/>
+        <textarea
+          name='message'
+          autoComplete='off'
+          className={styles.message_box}
+          style={{height: `${lineCount * 1.5}rem`}}
+          value={text}
+          onChange={handleChange}
+          required
+        />
         <button type='submit' className={styles.send_button}>Send</button>
       </form>
     </div>
