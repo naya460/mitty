6fa6c076935f74cc3c35e51413206f806c735dfa
naya460 <76655448+naya460@users.noreply.github.com>
2023-07-14 21:41:28 +0900

    websocketの接続を最初の1回だけにする

--- components/common/useWebSocket.ts
+++ components/common/useWebSocket.ts
@@ -1,35 +1,37 @@
-import { useState, useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
+let socket: WebSocket = null;
+let cookie: String = null;
 let callbacks = [];
 
 export default function useWebSocket(callback? : (message) => void): [(message) => void, String] {
-    const socket = useRef<WebSocket>(null);
-    const [cookie, setCookie] = useState<String>(null);
+  useEffect(() => {
+    // コールバック関数を追加
+    if (callback) {
+      callbacks.push(callback);
+    }
 
-    useEffect(() => {
-      if (callback) {
-        callbacks.push(callback);
+    // WebSocketが用意されていないとき、作成
+    if (socket != null) return;
+    (async () => {
+      const a = await fetch('api/use_ws');
+      cookie = await a.text();
+    })();
+    socket = new WebSocket(`ws://${location.hostname}:8080/`);
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      callbacks.forEach(f => {
+        f(data);
+      })
+    }
+  }, []);
+
+  return [
+    (message: Object): void => {
+      if (socket.OPEN) {
+        socket.send(JSON.stringify(message));
       }
-
-      (async () => {
-        const a = await fetch('api/use_ws');
-        setCookie(await a.text());
-        socket.current = new WebSocket(`ws://${location.hostname}:8080/`);
-  
-        socket.current.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          callbacks.forEach(f => {
-            f(data);
-          })
-        }
-      })();
-    }, []);
-  
-    return [
-      (message: Object): void => {
-        if (socket.current.OPEN) {
-          socket.current.send(JSON.stringify(message));
-        }
-      }, cookie
-    ];
-  }
+    }, cookie
+  ];
+}
