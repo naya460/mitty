7b77259b2fb0fcf9e90e880fe75452b14c223e96
naya460 <76655448+naya460@users.noreply.github.com>
2023-07-16 22:48:46 +0900

    メッセージをvanilla-extractを用いて装飾

---
+++ components/main_page/group_contents/message_list/message.css.ts
@@ -0,0 +1,37 @@
+import { style } from "@vanilla-extract/css"
+
+export default {
+  top: style({
+    width: "fit-content",
+    maxWidth: "70%",
+    minWidth: "1rem",
+    marginTop: "0.5rem",
+  }),
+  top_mine: style({
+    marginLeft: "auto",
+  }),
+  name: style({
+    marginLeft: "0.5rem",
+    marginRight: "0.5rem",
+    fontFamily: "sans-serif",
+    color: "black",
+  }),
+  name_mine: style({
+    textAlign: "right",
+  }),
+  message_box: style({
+    backgroundColor: "#cfe0cf",
+    whiteSpace: "pre-line",
+    padding: "0.5rem 1rem",
+    borderRadius: "0.5rem",
+    overflowWrap: "break-word",
+    fontFamily: "sans-serif",
+    color: "black",
+  }),
+  message_box_mine: style({
+    borderBottomRightRadius: "0",
+  }),
+  message_box_member: style({
+    borderBottomLeftRadius: "0",
+  }),
+};
--- components/main_page/group_contents/message_list/message.tsx
+++ components/main_page/group_contents/message_list/message.tsx
@@ -1,7 +1,7 @@
 import react from 'react'
 import {DateTime} from 'luxon'
 
-import styles from './message.module.css'
+import styles from './message.css'
 
 interface Props {
   user_name: string,
@@ -15,15 +15,15 @@ export default function Message(props: Props) {
   const time = DateTime.fromJSDate(new Date(props.time));
 
   return (
-    <div className={`${styles.top} ${(props.mine) && styles.mine_top}`}>
+    <div className={`${styles.top} ${(props.mine) && styles.top_mine}`}>
       {/* user name and time*/}
       <div
-        className={`${styles.name} ${(props.mine) && styles.mine_name}`}
+        className={`${styles.name} ${(props.mine) && styles.name_mine}`}
       >{props.user_name} [{time.toFormat("yyyy/MM/dd HH:mm")}]</div>
       {/* message box */}
       <div className={`
         ${styles.message_box}
-        ${(props.mine)? styles.mine_message_box : styles.member_message_box}
+        ${(props.mine)? styles.message_box_mine : styles.message_box_member}
       `}>
         {props.children}
       </div>
