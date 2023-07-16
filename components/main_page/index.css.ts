53fc03216a4d46f32e0c4b41f9dfe7e07955ec45
naya460 <76655448+naya460@users.noreply.github.com>
2023-07-16 22:29:46 +0900

    .css.tsのインデントをスペース2つに統一

--- components/main_page/index.css.ts
+++ components/main_page/index.css.ts
@@ -1,17 +1,17 @@
 import { style } from "@vanilla-extract/css"
 
 export default {
-    top: style({
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        flexBasis: "1",
-        flexGrow: "1",
-    }),
-    main_view: style({
-        width: "100%",
-        height: "100%",
-        display: "flex",
-    }),
+  top: style({
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    flexBasis: "1",
+    flexGrow: "1",
+  }),
+  main_view: style({
+    width: "100%",
+    height: "100%",
+    display: "flex",
+  }),
 };
--- components/main_page/main_menu/group_list/group.css.ts
+++ components/main_page/main_menu/group_list/group.css.ts
@@ -1,48 +1,48 @@
 import { style } from "@vanilla-extract/css"
 
 export default {
-    top: style({
-        position: "relative",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "3rem",
-        padding: "0.5rem",
-        marginTop: "0.2rem",
-        border: "0",
-        borderRadius: "0.25rem",
-        fontSize: "1rem",
-        fontFamily: "sans-serif",
-        color: "black",
-    }),
-    top_not_selected: style({
-        backgroundColor: "#f0f0f0",
-        "@media" : {
-            "screen and (hover: hover)": {
-                ":hover": {
-                    backgroundColor: "#dff0df",
-                },
-            },
-        },
-    }),
-    top_selected: style({
-        backgroundColor: "#cfe0cf",
-        ":hover": {
-            backgroundColor: "#cfe0cf",
-        },
-        "@media": {
-            "screen and (max-width: 40rem)": {
-                backgroundColor: "#f0f0f0",
-                ":hover": {
-                    backgroundColor: "#f0f0f0",
-                },
-            },
-        },
-    }),
-    count: style({
-        position: "absolute",
-        right: "1rem",
-    }),
+	top: style({
+		position: "relative",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		width: "100%",
+		height: "3rem",
+		padding: "0.5rem",
+		marginTop: "0.2rem",
+		border: "0",
+		borderRadius: "0.25rem",
+		fontSize: "1rem",
+		fontFamily: "sans-serif",
+		color: "black",
+	}),
+	top_not_selected: style({
+		backgroundColor: "#f0f0f0",
+		"@media" : {
+			"screen and (hover: hover)": {
+				":hover": {
+					backgroundColor: "#dff0df",
+				},
+			},
+		},
+	}),
+	top_selected: style({
+		backgroundColor: "#cfe0cf",
+		":hover": {
+			backgroundColor: "#cfe0cf",
+		},
+		"@media": {
+			"screen and (max-width: 40rem)": {
+				backgroundColor: "#f0f0f0",
+				":hover": {
+					backgroundColor: "#f0f0f0",
+				},
+			},
+		},
+	}),
+	count: style({
+		position: "absolute",
+		right: "1rem",
+	}),
 };
