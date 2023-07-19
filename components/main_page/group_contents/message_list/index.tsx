37e87e8dddd981238ba8d2230bb485134fd49cc2
naya460 <76655448+naya460@users.noreply.github.com>
2023-07-19 22:02:52 +0900

    メッセージや日付の表示処理を関数化

--- components/main_page/group_contents/message_list/index.tsx
+++ components/main_page/group_contents/message_list/index.tsx
@@ -20,25 +20,83 @@ export default function MessageList(props: Props) {
 
   selected_group_id.current = props.selected_group_id;
 
+  // 表示を追加する
+  const addDisplay = (
+    group_id: string,
+    element: JSX.Element
+  ) => {
+    // グループを読み込んでいないとき作成
+    if (!message_list.current.has(group_id)) {
+      message_list.current.set(group_id, []);
+    }
+
+    // 表示を追加
+    message_list.current.set(
+      group_id,
+      [element, ...message_list.current.get(group_id)]
+    );
+  }
+
+  // メッセージを追加する
+  const addMessage = (
+    message: {
+      group_id: string,
+      message_text: string,
+      author: { user_name: string},
+      time: Date,
+    },
+  ) => {
+    // グループを読み込んでいないとき作成
+    if (!message_list.current.has(message.group_id)) {
+      message_list.current.set(message.group_id, []);
+    }
+
+    // メッセージの表示を作成
+    const tmp = (
+      <Message
+        key={message_list.current.get(message.group_id).length}
+        user_name={message.author.user_name}
+        mine={props.user_name == message.author.user_name}
+        time={message.time}
+      >
+        {message.message_text}
+      </Message>
+    );
+
+    // 表示を追加
+    addDisplay(message.group_id, tmp);
+  }
+
+  // 日付を追加する
+  const addDate = (
+    group_id: string,
+    date: Date,
+  ) => {
+    // グループを読み込んでいないとき作成
+    if (!message_list.current.has(group_id)) {
+      message_list.current.set(group_id, []);
+    }
+    
+    // 日付の表示を作成
+    const tmp = (
+      <div key={message_list.current.get(group_id).length} className={styles.date}>
+        <div className={styles.date_hline} />
+        <div className={styles.date_text}>{date.toLocaleDateString()}</div>
+        <div className={styles.date_hline} />
+      </div>
+    );
+
+    // 表示を追加
+    addDisplay(group_id, tmp);
+  }
+
   useWebSocket((message) => {
     // グループのメッセージを読み込んでいないとき、無視
     if (!message_list.current.has(message.group_id)) {
       return;
     }
 
-    message_list.current.set(
-      message.group_id,
-      [(
-        <Message
-          key={message_list.current.get(message.group_id).length}
-          user_name={message.author.user_name}
-          mine={props.user_name == message.author.user_name}
-          time={message.time}
-        >
-          {message.message_text}
-        </Message>
-      ), ...message_list.current.get(message.group_id)]
-    );
+    addMessage(message);
 
     if (selected_group_id.current === message.group_id) {
       setDisplayMessages(() => {
@@ -80,41 +138,19 @@ export default function MessageList(props: Props) {
       // 日付が変わったとき
       if (before_date != null) {
         if (before_date != date.toLocaleDateString()) {
-          tmp.unshift(
-            <div key={offset} className={styles.date}>
-              <div className={styles.date_hline} />
-              <div className={styles.date_text}>{date.toLocaleDateString()}</div>
-              <div className={styles.date_hline} />
-            </div>
-          );
+          addDate(props.selected_group_id, date);
           offset++;
         }
       } else {
-        tmp.unshift(
-          <div key={offset} className={styles.date}>
-            <div className={styles.date_hline} />
-            <div className={styles.date_text}>{date.toLocaleDateString()}</div>
-            <div className={styles.date_hline} />
-          </div>
-        );
+        addDate(props.selected_group_id, date);
         offset++;
       }
       before_date = date.toLocaleDateString();
 
       // メッセージを追加
-      tmp.unshift(
-        <Message
-          key={offset}
-          user_name={messages[i].author.user_name}
-          mine={props.user_name == messages[i].author.user_name}
-          time={messages[i].time}
-        >
-          {messages[i].message_text}
-        </Message>
-      );
+      addMessage({...messages[i], group_id: props.selected_group_id});
       offset++;
     }
-    message_list.current.set(selected_group_id.current, tmp);
 
     // 最新のメッセージまでスクロール
     ref_messages_div.current.scrollTo(0, 0);
