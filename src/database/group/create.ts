3b973a5c3ef7ea702953f80000d219f431ae907d
naya460 <76655448+naya460@users.noreply.github.com>
2023-10-13 21:22:28 +0900

    #2 グループの作成処理を、databaseディレクトリ内に作成

---
+++ src/database/group/create.ts
@@ -0,0 +1,48 @@
+import prisma from 'lib/prisma';
+
+import getUserId from 'database/user/get_user_id';
+
+// # createGroup
+//   グループを作成する
+//
+// ## 引数
+//   - user_name : string
+//     グループの作成者のユーザー名
+//   - group_name : string
+//     追加するグループ名
+//
+// ## 返り値
+//   - Promise<boolean>
+//     - 作成に成功した場合 : true
+//     - 作成に失敗した場合 : false
+//
+// ### 条件
+//   - 作成者のユーザー名が不正なとき、失敗する
+//
+// ## 注意
+//   - APIから認証を済ませておくこと
+
+export default async function createGroup(
+  user_name: string,
+  group_name: string,
+): Promise<boolean> {
+  // ユーザーIDを取得
+  const user_id = await getUserId(user_name);
+  if (!user_id) {
+    return false;
+  }
+
+  // グループを追加
+  await prisma.group.create({
+    data: {
+      group_name: group_name,
+      members: {
+        create: [{
+          user_id: user_id,
+        }],
+      },
+    },
+  });
+
+  return true;
+}
--- src/pages/api/group/create.ts
+++ src/pages/api/group/create.ts
@@ -1,39 +1,23 @@
-import prisma from 'lib/prisma'
 import { withUserRoute } from 'lib/withSession'
 import { NextApiRequest, NextApiResponse } from 'next';
+
+import createGroup from 'database/group/create';
 
 // サインインしているときで、POSTリクエストのときのみ実行
 export default withUserRoute(CreateRoute, 'POST');
 
 async function CreateRoute(req: NextApiRequest, res: NextApiResponse) {
   // ユーザー名を入手
-  const user_name: string = req.session.user.user_name;
-  
-  // ユーザーIDを取得
-  const user_id = (await prisma.user.findUnique({
-    select: { user_id: true },
-    where: { user_name: user_name}
-  })).user_id;
-  if (user_id === null) {
-    res.status(500).send('Could not find the user.');
-    return;
-  }
+  const user_name = req.session.user.user_name;
 
   // グループ名を取得
-  const group_name: string = req.body.group_name;
-  if (group_name === null) {
-    res.status(400).send('The group_name did not exist in the request.');
+  const group_name = req.body.group_name;
+  if (group_name == null) {
+    res.status(400).end();
   }
 
   // グループを追加
-  await prisma.group.create({
-    data: {
-      group_name: group_name,
-      members: {
-        create: [{
-          user_id: user_id
-        }]
-      }
-    }
-  });
+  if (!(await createGroup(user_name, group_name))) {
+    res.status(400).end();
+  }
 }
