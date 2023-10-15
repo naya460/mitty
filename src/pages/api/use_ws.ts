eff21604e39f5a77e5b45068af7b6d7b5de28023
naya460 <76655448+naya460@users.noreply.github.com>
2023-10-15 14:55:00 +0900

    #2 Cookieの保存処理を、databaseディレクトリ内に作成

--- src/pages/api/use_ws.ts
+++ src/pages/api/use_ws.ts
@@ -1,19 +1,28 @@
 import { withUserRoute } from 'lib/withSession'
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from 'lib/prisma'
+
+import setUserCookie from 'database/user/set_cookie';
 
 // サインインしているときで、GETリクエストのときのみ実行
 export default withUserRoute(UseWsRoute, 'GET');
 
 async function UseWsRoute(req: NextApiRequest, res: NextApiResponse) {
-    await prisma.user.update({
-        data: {
-            cookie: Object.values(req.cookies)[0]
-        },
-        where: {
-            user_name: req.session.user.user_name
-        }
-    })
+    // ユーザー名を取得
+    const user_name = req.session.user.user_name;
+    if (!user_name) {
+        res.status(400).end();
+        return;
+    }
+
+    // cookieを取得
+    const cookie = Object.values(req.cookies)[0];
+    if (!cookie) {
+        res.status(400).end();
+    }
+
+    // クッキーを保存
+    await setUserCookie(user_name, cookie);
+
     res.status(200).send(Object.values(req.cookies)[0]);
     res.end();
 }
