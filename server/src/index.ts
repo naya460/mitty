7b599531214f61755ee1c5b4f3497650261dde15
naya460 <76655448+naya460@users.noreply.github.com>
2023-10-18 12:10:44 +0900

    #16 session-idをhttpOnlyのcookieとして保存する

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,24 @@
 import fastify from 'fastify';
 import { v4 as uuid_v4 } from 'uuid';
+import fastifyCookie from '@fastify/cookie';
 
 const server = fastify({
   logger: true,
 });
 
+server.register(fastifyCookie, {
+  secret: 'z8Cpj2nNv9VPdsxZxostDu5ufXxme2V0',
+});
+
 server.get('/', async (request, reply) => {
-  reply.type('application/json').code(200);
-  return { uuid: uuid_v4() };
+  console.log(request.cookies);
+  reply
+    .setCookie('session-id', uuid_v4(), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    })
+    .type('application/json').code(200)
+    .send({ test: 'ok' });
 });
 
 server.listen({ port: 9090, host: '0.0.0.0' }, (err, address) => {
