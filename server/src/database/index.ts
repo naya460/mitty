0e1a2d35c8c7e17fb1444bc4f3ed570450811cda
naya460 <76655448+naya460@users.noreply.github.com>
2023-11-13 15:48:35 +0900

    server側で使われていないdatabase apiを削除

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
-import createUser from './user/create';
 import getUserHash from './user/get_hash';
 import getUserId from './user/get_user_id';
 import setUserCookie from './user/set_cookie';
-import hasMember from './group/has_member';
 import getGroup from './group/get';
 import createGroup from './group/create';
-import groupExists from './group/exists';
 import getGroupMember from './group/member/get';
 import addGroupMember from './group/member/add';
 import getMessage from './message/get';
@@ -17,13 +14,6 @@ export default function databaseRoutes(
   opts: FastifyPluginOptions,
   done: (err?: Error | undefined) => void
 ): void {
-  server.post<{ Body: { user_name: string, hash: string }}>(
-    '/user/create',
-    async (req, res) => {
-      await createUser(req.body.user_name, req.body.hash);
-    }
-  );
-  
   server.post<{ Body: { user_name: string } }>(
     '/user/get_hash',
     async (req, res) => {
@@ -49,15 +39,6 @@ export default function databaseRoutes(
     }
   );
 
-  server.post<{ Body: { user_name: string, group_id: string } }>(
-    '/group/has_member',
-    async (req, res) => {
-      const exists = await hasMember(req.body.user_name, req.body.group_id);
-      res.type('application/json');
-      return { exists };
-    }
-  );
-
   server.post<{ Body: { user_name: string } }>(
     '/group/get',
     async (req, res) => {
@@ -72,15 +53,6 @@ export default function databaseRoutes(
     async (req, res) => {
       const ok = await createGroup(req.body.user_name, req.body.group_name);
       if (ok) res.status(201); else res.status(400);
-    }
-  );
-
-  server.post<{ Body: { group_id: string } }>(
-    '/group/exists',
-    async (req, res) => {
-      const exists = await groupExists(req.body.group_id);
-      res.type('application/json');
-      return { exists };
     }
   );
 
