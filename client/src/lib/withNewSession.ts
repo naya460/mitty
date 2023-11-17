510c80e403eecdd4765ac0a7a614a3b2f60e77f9
naya460 <76655448+naya460@users.noreply.github.com>
2023-11-18 08:31:50 +0900

    #15 clientのカスタムサーバーを削除

--- client/src/lib/withNewSession.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Redis } from 'ioredis';
-import { getCookie } from 'cookies-next';
-
-const redis = new Redis();
-
-export default async function authNewSession(req: NextApiRequest, res: NextApiResponse): Promise<string> {
-  return (await redis.hget('session', getCookie('session_id', { req, res })));
-}
