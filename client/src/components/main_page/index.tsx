fadbc598c8caf35f2a9883f0b3cdad679e3fd2c6
naya460 <76655448+naya460@users.noreply.github.com>
2023-12-09 01:52:20 +0900

    #25 ページ読み込み時に、ダークモードが反映されない問題を修正

--- client/src/components/main_page/index.tsx
+++ client/src/components/main_page/index.tsx
@@ -20,9 +20,17 @@ export default function MainPage(props: Props) {
 
   // const dark_mode = window.matchMedia('(prefers-color-scheme: dark)').matches;
   useEffect(() => {
+    // ダークモードのクエリを取得
     const dark_mode_query = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // モードを設定
+    setDarkmode(dark_mode_query.matches);
+
+    // リスナーを登録
     const listener = (event) => { setDarkmode(event.matches) };
     dark_mode_query.addEventListener('change', listener);
+
+    // リスナーの解除を返却
     return () => {
       dark_mode_query.removeEventListener('change', listener);
     }
