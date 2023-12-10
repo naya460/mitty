8f8f8678322dcc36b50fbc2f9024b5e1311b4606
naya460 <76655448+naya460@users.noreply.github.com>
2023-12-10 13:45:43 +0900

    message_inputの不要なstyleを削除

--- client/src/components/main_page/group_contents/message_input.css.ts
+++ client/src/components/main_page/group_contents/message_input.css.ts
@@ -16,22 +16,7 @@ export default {
   }),
   message_box: style({
     width: "100%",
-    height: "1.5rem",
-    resize: "none",
-    border: "1px",
-    borderStyle: "solid",
-    borderColor: gvars.color.main._3,
-    borderRadius: "0.5rem",
-    backgroundColor: gvars.color.base.light,
     margin: "0.2rem 0.5rem",
-    fontFamily: "sans-serif",
-    fontSize: "1rem",
-    color: gvars.color.font._0,
-    padding: "0.5rem",
-    lineHeight: "1.5rem",
-    ":focus-visible": {
-      outline: "none",
-    },
   }),
   send_button: style({
     border: "0",
